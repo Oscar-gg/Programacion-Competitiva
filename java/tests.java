97d314263413af37dbd0ad6ef69807d497293c04
Oscar-gg <oscar.arreola.jr@gmail.com>
2022-08-27 19:03:57 -0500

    Changes in tests.java

--- java/tests.java
+++ java/tests.java
@@ -4,6 +4,7 @@ import java.io.*;
 public class tests {
     public static void main(String[] args) throws FileNotFoundException {
         Scanner sc;
+        /* 
         try {
             sc = new Scanner(new File("input.txt"));
             PrintStream out = new PrintStream(new FileOutputStream("output.txt"));
@@ -14,6 +15,14 @@ public class tests {
         while (sc.hasNextLine()) {
             // problem();
         }
+        */
+
+        //int a = 4; // 0100
+        //int b = 5;// 1010
+
+        int c =  13456 << 21;
+
+        System.out.println(c);
     }
 
     public static void problem() {
