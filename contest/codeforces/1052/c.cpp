7f52f2e87896e50d91f8f8dea7a2e9c70168b398
Oscar Arreola <oscar.arreola.jr@gmail.com>
2025-09-27 19:52:41 -0700

    contest

---
+++ contest/codeforces/1052/c.cpp
@@ -0,0 +1,104 @@
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <deque>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <limits.h>
+#include <map>
+#include <math.h>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <utility>
+#include <vector>
+#include <unordered_set>
+#include <unordered_map>
+
+#define PI 3.141592653589793
+#define EPS 0.000000001
+#define INF 1000000000
+
+#define _ ios_base::sync_with_stdio(0), cin.tie(0), cin.tie(0), cout.tie(0), cout.precision(15);
+#define FOR(i, a, b) for (int i = int(a); i < int(b); i++)
+#define RFOR(i, a, b) for (int i = int(a) - 1; i >= int(b); i--)
+#define FORC(cont, it) for (typeof((cont).begin()) it = (cont).begin(); it != (cont).end(); it++)
+#define RFORC(cont, it) for (typeof((cont).rbegin()) it = (cont).rbegin(); it != (cont).rend(); it++)
+#define pb push_back
+#define dbg(v) cout << "Line(" << __LINE__ << ") -> " << #v << " = " << (v) << endl;
+#define all(x) x.begin(), x.end()
+#define nl << "\n"
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+
+#define MAXN 10
+#define MOD 1000000007
+
+void s()
+{
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    vector<int> permut(n, 0);
+    iota(all(permut), 1);
+
+    int r = 0;
+    while (r < n)
+    {
+        if (s[r] == '0')
+        {
+            int l = r;
+            while (r < n && s[r] == '0')
+                r++;
+
+            r--;
+
+            if (r - l == 0)
+            {
+                cout << "NO\n";
+                return;
+            }
+            int cr = r + 1;
+            for (int i = l; i <= r; i++)
+            {
+                permut[i] = cr--;
+            }
+        }
+        r++;
+    }
+
+    cout << "YES\n";
+    for (auto p : permut)
+    {
+        cout << p << " ";
+    }
+    cout nl;
+}
+
+int main()
+{
+    _;
+
+    // freopen("file.in", "r", stdin);
+    // freopen("file.out", "w", stdout);
+    int t = 1;
+    cin >> t;
+    while (t--)
+        s();
+    return 0;
+}
